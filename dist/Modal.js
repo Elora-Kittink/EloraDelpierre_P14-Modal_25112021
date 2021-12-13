0c67657306b55160720537e694293faccf064249
Elora <elora.delpierre@gmail.com>
2021-12-13 18:07:41 +0100

    sup css

--- dist/Modal.js
+++ dist/Modal.js
@@ -3,29 +3,40 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _modal = _interopRequireDefault(require("./modal.css"));
-
 var Modal = function Modal(_ref) {
   var isOpen = _ref.isOpen,
-      handleModalOpening = _ref.handleModalOpening;
+    handleModalOpening = _ref.handleModalOpening;
 
   if (!isOpen) {
     return null;
   }
 
-  return /*#__PURE__*/_react.default.createElement("div", {
-    className: "flex-modal"
-  }, /*#__PURE__*/_react.default.createElement("div", {
-    className: "modal"
-  }, /*#__PURE__*/_react.default.createElement("button", {
-    onClick: handleModalOpening
-  }, "X"), /*#__PURE__*/_react.default.createElement("p", null, "Employ\xE9 cr\xE9\xE9 !")));
+  return /*#__PURE__*/ _react.default.createElement(
+    "div",
+    {
+      className: "flex-modal",
+    },
+    /*#__PURE__*/ _react.default.createElement(
+      "div",
+      {
+        className: "modal",
+      },
+      /*#__PURE__*/ _react.default.createElement(
+        "button",
+        {
+          onClick: handleModalOpening,
+        },
+        "X"
+      ),
+      /*#__PURE__*/ _react.default.createElement("p", null, "Employ\xE9 cr\xE9\xE9 !")
+    )
+  );
 };
 
 var _default = Modal;
